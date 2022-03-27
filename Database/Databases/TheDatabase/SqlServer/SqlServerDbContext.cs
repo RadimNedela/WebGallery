cf666f0c9ac4d3cb73a007fb0c70c08a80f2b909
Radim Neděla <radim.nedela@storaenso.com>
2022-03-27 13:02:44 +0200

    databáze zase funguje

--- Database/Databases/TheDatabase/SqlServer/SqlServerDbContext.cs
+++ Database/Databases/TheDatabase/SqlServer/SqlServerDbContext.cs
@@ -6,12 +6,12 @@ namespace WebGalery.Database.Databases.TheDatabase.SqlServer
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var connectionString =
-                "Data Source=(LocalDB)\\MSSQLLocalDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            //var connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            var connectionString = "Data Source=RADIMS;Initial Catalog=galery;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
             optionsBuilder
                 .EnableSensitiveDataLogging()
                 //.UseLoggerFactory(LoggerFactory)
-                .UseSqlServer(connectionString + ";database=galery");
+                .UseSqlServer(connectionString/* + ";database=galery"*/);
         }
     }
 }

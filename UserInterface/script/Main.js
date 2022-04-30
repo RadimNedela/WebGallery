9c27b9b4b55e34b09ed58679a628783a8e8a7113
Radim Neděla <radim.nedela@storaenso.com>
2022-04-30 11:28:55 +0200

    netuším, ale chci pokračovat

---
+++ UserInterface/script/Main.js
@@ -0,0 +1,15 @@
+﻿"use strict";
+
+function main() {
+    function getDatabase() {
+        $('#podTlacitkem').text("asdf");
+        //return { Database: "asdf" };
+    }
+
+    return {
+        GetDatabase: getDatabase,
+    };
+}
+
+if (!document.MainScript)
+    document.MainScript = main();
